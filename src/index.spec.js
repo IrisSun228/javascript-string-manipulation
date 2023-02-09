e8b74dc510e9a79c15573b46e0eb912fe5fc9005
Iris Sun <irissun@live.com>
2023-02-08 20:28:15 -0500

    Section 2 Step 7 - Test line transformations at the end of a string

--- src/index.spec.js
+++ src/index.spec.js
@@ -14,7 +14,7 @@ describe("CRLF Converter", () => {
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
 
-      // Use `String.prototype.endsWith()`
+      assert(result.endsWith("there.\n"));
     });
 
     it("should replace CRLF in the middle of a string", () => {
