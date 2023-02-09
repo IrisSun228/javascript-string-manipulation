f58cbb6d7cf5632d4ac8183a4b5b66514d50c577
Iris Sun <irissun@live.com>
2023-02-08 20:26:44 -0500

    Section 2 Step 6 - Test line transformations at the beginning of a string

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 const { describe, it } = require("mocha");
 const { lf } = global.lf || require("index.js");
+const { assert } = require("chai");
 
 describe("CRLF Converter", () => {
   describe("lf", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
 
-      // Use `String.prototype.startsWith()`
+      assert(result.startsWith("\nHello"));
     });
 
     it("should replace CRLF at the end of a string", () => {
